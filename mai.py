604670bdb27d6bbc21902cbca2785c683928b404
tayloroh98 <93334327+tayloroh98@users.noreply.github.com>
2021-10-28 19:56:31 -0600

    fun

---
+++ mai.py
@@ -0,0 +1,15 @@
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("Black")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import prettytable
+table = prettytable()
+print(table)
+
+
